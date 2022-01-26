da65c32f2022fde51ecaf0095986de517667e6dc
kumar113 <aman.kumar@knoldus.com>
2022-01-27 00:17:55 +0530

    Program 15

---
+++ SingletonClass.java
@@ -0,0 +1,26 @@
+class FormationOfSingletonClass
+{
+    private static FormationOfSingletonClass single=null;
+    public String str;
+    private FormationOfSingletonClass()
+    {
+        str="singleton class String";
+    }
+    public static FormationOfSingletonClass get()
+    {
+        if(single==null)
+            single = new FormationOfSingletonClass();
+        
+        return single;
+    }
+}
+
+public class SingletonClass{
+    public static void main(String[] args)
+    {
+        FormationOfSingletonClass value1= FormationOfSingletonClass.get();
+        FormationOfSingletonClass value2= FormationOfSingletonClass.get();
+        FormationOfSingletonClass value3= FormationOfSingletonClass.get();
+        System.out.println("value1: "+ value1.str + "\nvalue2: "+ value2.str +"\nvalue3: "+ value3.str);
+    }
+}
