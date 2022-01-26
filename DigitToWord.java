608de1d2aaf6aa2ba9e79fa4a4795da64319bff0
kumar113 <aman.kumar@knoldus.com>
2022-01-27 00:22:50 +0530

    Program 19

---
+++ DigitToWord.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+class NumbersInWords{
+    String[] oneword = { "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven",
+            "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
+    String[] multipleoften = { "", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+    String[] poweroften = { "", "Thousand", "Million", "Billion" , "Trillion" , "Quadrillion" ,"Quintillion"};
+
+    public String numberToWords(long digit ){
+        if (digit== 0)
+            return "Zero";
+        String result = "";
+        int i = 0;
+        while (digit > 0) {
+            if (digit% 1000 != 0) {
+                result = solve(digit % 1000) + poweroften[i] + " " + result;
+            }
+            digit/= 1000;
+            i++;
+        }
+        return result.trim();
+    }
+
+    public String solve(long digit) {
+        if (digit== 0)
+            return "";
+        if (digit< 20) {
+            return oneword[(int)(digit % 20)] + " ";
+        } else if (digit< 100) {
+            return multipleoften[(int)(digit / 10)] + " " + solve(digit% 10);
+        } else {
+            return oneword[(int)(digit / 100)] + " Hundred " + solve(digit % 100);
+        }
+    }
+    
+}
+
+public class DigitToWord{
+    public static void main(String[] args)
+    {   long n;
+        NumbersInWords num= new NumbersInWords();
+        Scanner ic= new Scanner(System.in);
+        try{
+            n=ic.nextLong();
+        }
+        finally{
+            ic.close();
+        }
+        System.out.println(num.numberToWords(n));
+    }
+}
